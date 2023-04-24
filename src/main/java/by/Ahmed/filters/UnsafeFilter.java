01d7979c97506c4f59412228634eaacbc374f1bf
Ahmed <83308467+Guardian-lang@users.noreply.github.com>
2023-04-24 22:04:56 +0300

    MyFirstProject

---
+++ src/main/java/by/Ahmed/filters/UnsafeFilter.java
@@ -0,0 +1,22 @@
+package by.Ahmed.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/admin")
+public class UnsafeFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var user = ((HttpServletRequest) servletRequest)
+                .getSession().getAttribute("author");
+        if (user != null) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            ((HttpServletResponse)servletResponse).sendRedirect("/registration");
+        }
+    }
+}
